7349a0ff70f43274e2ba6da1ceaedae9d08a3229
Vikram Panduranga <vpandura@codeaurora.org>
2020-03-23 13:20:40 -0700

    dsp: add support for voice call
    Add voice call support for kona.
    
    Change-Id: If87546f369503a8c551c4e2413b9d5c55115be82

--- dsp/Android.mk
+++ dsp/Android.mk
@@ -116,6 +116,13 @@ LOCAL_MODULE_DEBUG_ENABLE := true
 LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
 ###########################################################
+include $(CLEAR_VARS)
+LOCAL_MODULE              := $(AUDIO_CHIPSET)_voice_mhi.ko
+LOCAL_MODULE_KBUILD_NAME  := voice_mhi_dlkm.ko
+LOCAL_MODULE_TAGS         := optional
+LOCAL_MODULE_DEBUG_ENABLE := true
+LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
 ##########################################################
 ###########################################################
 
