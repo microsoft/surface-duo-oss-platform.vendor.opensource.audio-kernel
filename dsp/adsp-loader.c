96b6fbcdf9de1ccafe15e668bbd96f9385485653
Deru Wang <deruwang@codeaurora.org>
2020-08-28 13:45:30 +0800

    dsp: add new boot cmd for early audio
    The schedule_work of adsp loader boot will block machine driver probing
    need add new boot cmd without schedule_work to accelerate early audio
    
    Signed-off-by: Deru Wang <deruwang@codeaurora.org>

--- dsp/adsp-loader.c
+++ dsp/adsp-loader.c
@@ -29,6 +29,7 @@
 #define BOOT_CMD 1
 #define SSR_RESET_CMD 1
 #define IMAGE_UNLOAD_CMD 0
+#define BOOT_FOR_EARLY_CHIME_CMD 2
 
 static ssize_t adsp_boot_store(struct kobject *kobj,
 	struct kobj_attribute *attr,
@@ -220,7 +221,11 @@ static ssize_t adsp_boot_store(struct kobject *kobj,
 	} else if (boot == IMAGE_UNLOAD_CMD) {
 		pr_debug("%s: going to call adsp_unloader\n", __func__);
 		adsp_loader_unload(adsp_private);
+	} else if (boot == BOOT_FOR_EARLY_CHIME_CMD) {
+		pr_debug("%s: going to call adsp_load_fw\n", __func__);
+		adsp_load_fw(NULL);
 	}
+
 	return count;
 }
 
