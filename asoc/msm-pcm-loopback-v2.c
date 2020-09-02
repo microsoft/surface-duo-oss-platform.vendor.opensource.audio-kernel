5916ea9b689441c995b93473a0854fb24ede6d91
xsang <xsang@codeaurora.org>
2020-09-02 21:15:00 +0800

    asoc: add missing null check for volume control
    Pointer is not checked for null in volume control.
    Add missing check.
    
    Change-Id: I511a79422eaeced6240849cbb665a289afdc5984
    Signed-off-by: xsang <xsang@codeaurora.org>

--- asoc/msm-pcm-loopback-v2.c
+++ asoc/msm-pcm-loopback-v2.c
@@ -489,10 +489,21 @@ static int msm_pcm_volume_ctl_put(struct snd_kcontrol *kcontrol,
 {
 	int rc = 0;
 	struct snd_pcm_volume *vol = kcontrol->private_data;
-	struct snd_pcm_substream *substream = vol->pcm->streams[0].substream;
+	struct snd_pcm_substream *substream = NULL;
 	struct msm_pcm_loopback *prtd;
 	int volume = ucontrol->value.integer.value[0];
 
+	if (!vol) {
+		pr_err("%s: vol is NULL\n", __func__);
+		return -ENODEV;
+	}
+
+	if (!vol->pcm) {
+		pr_err("%s: vol->pcm is NULL\n", __func__);
+		return -ENODEV;
+	}
+
+	substream = vol->pcm->streams[0].substream;
 	pr_debug("%s: volume : 0x%x\n", __func__, volume);
 	if ((!substream) || (!substream->runtime)) {
 		pr_err("%s substream or runtime not found\n", __func__);
@@ -523,9 +534,13 @@ static int msm_pcm_volume_ctl_get(struct snd_kcontrol *kcontrol,
 	struct msm_pcm_loopback *prtd;
 
 	pr_debug("%s\n", __func__);
-
 	if (!vol) {
 		pr_err("%s: vol is NULL\n", __func__);
+		return -ENODEV;
+	}
+
+	if (!vol->pcm) {
+		pr_err("%s: vol->pcm is NULL\n", __func__);
 		return -ENODEV;
 	}
 
