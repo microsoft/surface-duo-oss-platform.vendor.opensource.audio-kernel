dcc3dee23f03edf670006e919db2320acf5d4da8
Saurav Kumar <sauravk@codeaurora.org>
2020-09-10 16:06:16 +0530

    dsp: add change to handle use-after-free in cal_utils_is_cal_stale
    Add change to address the race condition between
    pointer dereference and memory deallocation.
    
    Change-Id: Ia1ed47986ec81d3dc2feb3bc874847fadddac292
    Signed-off-by: Saurav Kumar <sauravk@codeaurora.org>

--- dsp/q6afe.c
+++ dsp/q6afe.c
@@ -2696,7 +2696,6 @@ static int send_afe_cal_type(int cal_index, int port_id)
 	else
 		cal_block = cal_utils_get_only_cal_block(
 				this_afe.cal_data[cal_index]);
-
 	if (cal_block == NULL || cal_utils_is_cal_stale(cal_block)) {
 		pr_err_ratelimited("%s cal_block not found!!\n", __func__);
 		ret = -EINVAL;
