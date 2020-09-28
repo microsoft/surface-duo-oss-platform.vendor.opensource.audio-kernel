31d60a3259ad615b946bc9dd0249830c22050e98
Sivasri Kumar Vanka <sivasri@codeaurora.org>
2020-09-28 21:42:33 +0530

    ASoC: msm-routing: Add lock in lsm_app_type_cfg_control callbacks
    Add routing_lock mutex to protect global variables in _get() & _put()
    callback of Listen App Type Config mixer control.
    
    Change-Id: Id17ee80fce83bdbc6a7371de2aaf3c92fe587caa
    Signed-off-by: Sivasri Kumar Vanka <sivasri@codeaurora.org>

--- 4.0/asoc/msm-pcm-routing-v2.c
+++ 4.0/asoc/msm-pcm-routing-v2.c
@@ -21673,6 +21673,7 @@ static int msm_routing_get_lsm_app_type_cfg_control(
 				kcontrol->private_value)->shift;
 	int i = 0, j = 0;
 
+	mutex_lock(&routing_lock);
 	ucontrol->value.integer.value[i] = num_app_cfg_types;
 
 	for (j = 0; j < num_app_cfg_types; ++j) {
@@ -21686,6 +21687,7 @@ static int msm_routing_get_lsm_app_type_cfg_control(
 			ucontrol->value.integer.value[++i] =
 				lsm_app_type_cfg[j].num_out_channels;
 	}
+	mutex_unlock(&routing_lock);
 	return 0;
 }
 
@@ -21699,9 +21701,11 @@ static int msm_routing_put_lsm_app_type_cfg_control(
 
 	int num_app_types;
 
+	mutex_lock(&routing_lock);
 	if (ucontrol->value.integer.value[0] > MAX_APP_TYPES) {
 		pr_err("%s: number of app types exceed the max supported\n",
 			__func__);
+	mutex_unlock(&routing_lock);
 		return -EINVAL;
 	}
 
@@ -21721,7 +21725,7 @@ static int msm_routing_put_lsm_app_type_cfg_control(
 			lsm_app_type_cfg[j].num_out_channels =
 				ucontrol->value.integer.value[i++];
 	}
-
+	mutex_unlock(&routing_lock);
 	return 0;
 }
 
